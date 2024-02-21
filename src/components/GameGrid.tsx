da5161200a64cf6220ad4216f296e2f47ab78bcf
ArcL <lou.arc.la@gmail.com>
2024-02-20 23:08:34 -0600

    Equal distribuiton amongst cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,23 +5,23 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const { data, error, isLoading } = useGames();
+  const skeletons = 15;
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={3}
         padding="10px"
       >
         {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSkeleton key={skeleton} />
+          Array.from({ length: skeletons }).map((_, i) => (
+            <GameCardContainer key={i}>
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>

8e177216ddd128b998ba419da4725bbd4489ce9d
ArcL <lou.arc.la@gmail.com>
2024-02-24 22:47:13 -0600

    Clean Up Game Cards

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -22,7 +22,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
 
   return (
     <>
-      <Heading fontSize="2xl" marginBottom={3}>
+      <Heading fontSize="2xl" marginBottom={1}>
         Genres
       </Heading>
       <List>

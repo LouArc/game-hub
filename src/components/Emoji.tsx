104d863ad73854e2119892543f7aa5d48cf2183b
ArcL <lou.arc.la@gmail.com>
2024-02-26 16:15:35 -0600

    Thums down emji

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -2,6 +2,7 @@ import { Image, ImageProps } from "@chakra-ui/react";
 import bullsEye from "../assets/bulls-eye.webp";
 import thumbsUp from "../assets/thumbs-up.webp";
 import meh from "../assets/meh.webp";
+import thumbsDown from "../assets/thumbs-down.webp";
 
 interface Props {
   rating: number;
@@ -12,6 +13,8 @@ const Emoji = ({ rating }: Props) => {
 
   const emojiMap: { [key: number]: ImageProps } = {
     //index signature
+    1: { src: thumbsDown, alt: "Not recomended", boxSize: "25px" },
+    2: { src: thumbsDown, alt: "Not recomended", boxSize: "25px" },
     3: { src: meh, alt: "meh", boxSize: "25px" },
     4: { src: thumbsUp, alt: "recomended", boxSize: "25px" },
     5: { src: bullsEye, alt: "expecional", boxSize: "35px" },
